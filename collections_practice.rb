3abc059e129063cfa29435d46549c0788de08319
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 03:27:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -74,7 +74,7 @@ def add_s(array)
   # Accepts an array of strings.
   # Return an array with an "s" added to each word, except "feet".
   array.each_with_index.collect do |word, index|
-    index == 2 ? "#{word}" : "#{word}s"
+    index == 1 ? "#{word}" : "#{word}s"
   end
   
 end
