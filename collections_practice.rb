f0725a3d6b0ef44201eb59bd74cc70719cdcc041
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 02:44:33 +0000

    Automatically backed up by Learn

---
+++ collections_practice.rb
@@ -0,0 +1,57 @@
+def sort_array_asc(array)
+  # Accepts an array of integers.
+  # Returns a copy array with the intergers in ascending order.
+  
+end
+
+
+def sort_array_desc(array)
+  # Accepts an array of integers.
+  # Returns a copy array with the integers in decsending order.
+  
+end
+
+
+def sort_array_char_count(array)
+  # Accepts an array of strings.
+  # Returns a copy array, ordered from shortest to longest string.
+  
+end
+
+
+def swap_elements(array)
+  # Accepts an array of any data types.
+  # Returns the array with the 2nd & 3rd elements swapped.
+  
+end
+  
+  
+def reverse_array(array)
+  # Accepts an array of integers.
+  # Returns a copy array with the elements in reverse order.
+  
+end
+
+
+def kesha_maker(array)
+  # Accepts an array of strings.
+  # Replaces the 3rd character in each string with "$"
+  
+end
+
+
+def find_a(array)
+  # Accepts an array of strings.
+  # Returns all strings that start with "A"
+  # Utilizes a high level iterator. (Inherent return.)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
